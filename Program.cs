44987e1458342970cceb8e19a0f3582a375f7a9c
billygrahan <billygrahan@alu.ufc.br>
2024-11-17 22:46:44 -0300

    teste inicial da api auxiliar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// API Key: f7061918278fce76aaafa463
+using System;
+using Newtonsoft.Json;
+using Conversor_de_Moedas;
+
+try
+{
+    String URLString = "https://v6.exchangerate-api.com/v6/f7061918278fce76aaafa463/pair/CAD/BRL/2.0000";
+    using (var webClient = new System.Net.WebClient())
+    {
+        var json = webClient.DownloadString(URLString);
+        API_Obj Test = JsonConvert.DeserializeObject<API_Obj>(json);
+        Console.WriteLine(Test.conversion_result);
+    }
+}
+catch (Exception)
+{
+    Console.WriteLine("erro!");
+}
--- obj/Debug/net8.0/Conversor-de-Moedas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Conversor-de-Moedas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conversor-de-Moedas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07fd61adb5e17d67feafade3244022e8a94de2be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conversor-de-Moedas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conversor-de-Moedas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
